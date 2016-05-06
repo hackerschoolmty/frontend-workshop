89cf1a9525b537ac444bdded05a63bae97e99ef7
Fernando Flores <ferflores@gmail.com>
2016-05-05 19:36:38 -0500

    calculator as jquery component

--- js/calculator.js
+++ js/calculator.js
@@ -1,19 +1,42 @@
-$(document).ready(onDocumentReady)
+$.fn.calculator = function(){
 
-function onDocumentReady(){
+  var html = "<table class='table'>"
+            +"  <tr>"
+            +"    <td>"
+            +"      Number 1:"
+            +"    </td>"
+            +"    <td>"
+            +"      <input id='number1' class='form-control' type='text'>"
+            +"    </td>"
+            +"  </tr>"
+            +"  <tr>"
+            +"    <td>"
+            +"      Number 2:"
+            +"    </td>"
+            +"    <td>"
+            +"      <input id='number2' class='form-control' type='text'>"
+            +"    </td>"
+            +"  </tr>"
+            +"  <tr>"
+            +"    <td>"
+            +"      <button id='addButton' class='btn btn-success' type='button'>Add</button>"
+            +"    </td>"
+            +"    <td>"
+            +"      <input id='result' class='form-control' type='text' readonly='readonly'>"
+            +"    </td>"
+            +"  </tr>"
+            +"</table>";
 
-  function add(){
-    //var number1 = document.getElementById('number1').value;
-    var number1 = $('#number1').val();
-    var number2 = $('#number2').val();
-    var result = parseInt(number1) + parseInt(number2);
+    this.html(html);
 
-    $('#result').val(result);
-    $('#result').addClass('shake');
-  }
+    function add(){
+      var number1 = $('#number1').val();
+      var number2 = $('#number2').val();
+      var result = parseInt(number1) + parseInt(number2);
 
-  //var button = document.getElementById('addButton');
-  //button.addEventListener('click', add);
+      $('#result').val(result);
+      $('#result').addClass('shake');
+    }
 
-  $('#addButton').on('click', add);
+    $('#addButton').on('click', add);
 }
